11a7712bf9f4750d602030a9edc66d3d99cddcf2
SanthiyaShankar <thiyasankar1406@gmail.com>
2020-10-10 11:59:25 +0530

    spring boot app initial commit

---
+++ EmployeeAssist/src/main/java/com/spring/guides/EmployeeAssistApplication.java
@@ -0,0 +1,13 @@
+package com.spring.guides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAssistApplication.class, args);
+	}
+
+}
